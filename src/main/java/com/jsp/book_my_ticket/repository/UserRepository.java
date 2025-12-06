d22d2da3c9033e6f0434ef79ce31717da9ca3089
Sowmya <sowmya2563@gmail.com>
2025-12-06 09:54:20 +0530

    Registration and Login

--- src/main/java/com/jsp/book_my_ticket/repository/UserRepository.java
+++ src/main/java/com/jsp/book_my_ticket/repository/UserRepository.java
@@ -9,5 +9,7 @@ public interface UserRepository extends JpaRepository<User, Long> {
 	boolean existsByEmail(String email);
 
 	void deleteByRole(String string);
+	
+	User findByEmail(String email);
 
 }
--- src/main/java/com/jsp/book_my_ticket/service/UserService.java
+++ src/main/java/com/jsp/book_my_ticket/service/UserService.java
@@ -1,6 +1,18 @@
 package com.jsp.book_my_ticket.service;
 
+import org.springframework.validation.BindingResult;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import com.jsp.book_my_ticket.dto.LoginDto;
+import com.jsp.book_my_ticket.dto.UserDto;
+
+import jakarta.servlet.http.HttpSession;
+
 public interface UserService {
+	
+	String register(UserDto userDto, BindingResult result);
+
+	String login(LoginDto dto, RedirectAttributes attributes,HttpSession session);
 
 
 }
