c6695671cf9def14a3d18166056d7d1454dfeede
Sowmya <sowmya2563@gmail.com>
2026-04-04 17:27:31 +0530

    Manage Users

--- src/main/java/com/jsp/book_my_ticket/util/EmailHelper.java
+++ src/main/java/com/jsp/book_my_ticket/util/EmailHelper.java
@@ -2,6 +2,7 @@ package com.jsp.book_my_ticket.util;
 
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.MimeMessageHelper;
+import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Component;
 import org.thymeleaf.TemplateEngine;
 import org.thymeleaf.context.Context;
@@ -15,7 +16,8 @@ public class EmailHelper {
 
 	private final JavaMailSender mailSender;
 	private final TemplateEngine templateEngine;
-
+	
+	@Async
 	public void sendOtp(int otp, String name, String email) {
 		MimeMessage mimeMessage = mailSender.createMimeMessage();
 		MimeMessageHelper helper = new MimeMessageHelper(mimeMessage);
